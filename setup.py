0ee63695e9c70f335ae2b304a2a83c8c72c90b83
Ivan Usalko <dk4aol@gmail.com>
2022-01-31 23:50:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION = "0.2.7"
+VERSION = "0.1.0a"
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 with io.open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="elasticsearch-dbapi",
-    description=("A DBAPI and SQLAlchemy dialect for Elasticsearch"),
+    name="sqlalchemy-es",
+    description=("SQLAlchemy dialect for Elasticsearch with support write operations"),
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=VERSION,
@@ -40,6 +40,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     tests_require=["nose>=1.0"],
     test_suite="nose.collector",
